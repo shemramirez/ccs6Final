e84dcee2aac65c0b7c9264599add98c4e59ed598
shemramirez <shemzramirez@gmail.com>
2020-11-30 12:16:31 +0800

    second

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>BLOG HOME</h1>')
+    return render(request, 'blog/home.html')
+
+def about(request):
+    return render(request, 'blog/about.html')
