837a8f7eaac8a4c7b6c65f5b8a85fe862ebec79d
shemramirez <shemzramirez@gmail.com>
2020-12-16 01:08:37 +0800

    new

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+
+'''
+posts = [
+    {
+        'author':'George',
+        'title':'Blog post 1',
+        'content':'First post content',
+        'date_posted':'August 27, 2018',
+    },
+    {
+        'author':'Jane',
+        'title':'Blog post 2',
+        'content':'second post content',
+        'date_posted':'August 28, 2018',
+    }
+]
+'''
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts':Post.objects.all()
+    }
+    return render(request, 'blog/home.html',context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html',{'title': 'About'})
+
+def performance(request):
+    return render(request, 'blog/performance.html')
