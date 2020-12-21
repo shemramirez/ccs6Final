2adf371371fadcc31cd6b46ab46c395615615cde
shemramirez <shemzramirez@gmail.com>
2020-12-22 00:08:59 +0800

    profile

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from .views import PostListView, PostDetailView
 from . import views
 
 urlpatterns = [
-    path('', views.home,name='blog-home'),
+    path('', PostListView.as_view(),name='blog-home'),
+    path('post/<int:pk>', PostDetailView.as_view(),name='post-detail'),
     path('about/', views.about,name='blog-about'),
-    path('performance/', views.performance,name='blog-performance')
+    path('performance/', views.performance,name='blog-performance'),
+    path('grades/', views.grades,name='blog-grades'),
+    path('chatroom/', views.chat, name='blog-chat'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-'''from django.contrib.auth.forms import Listview'''
-
-
-
+from django.db.models import Q
+from django.views.generic import ListView, DetailView
 
 '''
 posts = [
@@ -30,11 +28,48 @@ def home(request):
     }
     return render(request, 'blog/home.html',context)
 
+def grade(request):
+    context = {
+        'posts':Post.objects.all()
+    }
+    return render(request, 'blog/grades.html',context)
+
+class PostListView(ListView):
+    model = Post    
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post    
+
 def about(request):
     return render(request, 'blog/about.html',{'title': 'About'})
 
 def performance(request):
     return render(request, 'blog/performance.html')
+    
+def chat(request):
+    return render(request, 'blog/chat.html')
+
+def grades(request):
+    return render(request, 'blog/grades.html')
+
+def get_blog_query(query = None):
+    queryset = []
+    queries = query.split(" ") 
+    for q in queries:
+        post = Post.objects.filter(
+            Q(title__icontains=q) |
+            Q(content__icontains=q )
+        ).distinct()
+
+        for post in posts:
+            queryset.append(post)
+
+    return list(set(queryset))
+
+
 
 '''
 class SearchView(ListView):
@@ -52,18 +87,4 @@ class SearchView(ListView):
             result = None
         return result
 '''
-def get_blog_query(query = None):
-    queryset = []
-    queries = query.split(" ") 
-    for q in queries:
-        post = Post.objects.filter(
-            Q(title__icontains=q) |
-            Q(author__icontains=1)
-        ).distinct()
 
-        for post in posts:
-            queryset.append(post)
-
-    return list(set(queryset))
-
-    
